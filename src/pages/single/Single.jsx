ec7ddd8b9fff41780e4c7e409474825a13c05fbd
Inish <inish.bashyal99@gmail.com>
2023-03-07 10:47:36 +0545

    Some changes

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,5 +1,4 @@
 import "./single.css"
-import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import SinglePost from "../../components/singlePost/SinglePost"
 export default function Single() {
